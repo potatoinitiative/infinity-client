40459bec19c71655a46c6c674e973b6a084b74bb
Night <night@minepay.net>
2016-09-24 12:17:31 -0700

    scirpts

---
+++ fetch.sh
@@ -0,0 +1,5 @@
+git init
+git remote add origin https://github.com/FTBCommunity/infinity-client.git
+git fetch
+git checkout origin/master -ft
+
---
+++ init.sh
@@ -0,0 +1,7 @@
+rm -rf mods
+rm -rf config
+git init
+git remote add origin https://github.com/FTBCommunity/infinity-client.git
+git fetch
+git checkout origin/master -ft
+
